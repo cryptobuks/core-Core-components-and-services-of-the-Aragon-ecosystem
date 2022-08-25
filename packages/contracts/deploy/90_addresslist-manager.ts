3b4430092803542fd6bf206666164fa8ddb893c8
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-08-25 17:17:16 +0200

    update deploy script register plugin on repo

--- packages/contracts/deploy/90_addresslist-manager.ts
+++ packages/contracts/deploy/90_addresslist-manager.ts
@@ -1,5 +1,6 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
+import {getContractAddress} from './helpers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts, ethers} = hre;
@@ -7,10 +8,29 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const {deployer} = await getNamedAccounts();
 
-  await deploy('AddressListVotingManager', {
+  const ret = await deploy('AddressListVotingManager', {
     from: deployer,
     log: true,
   });
+
+  const addressListVotingManagerAddress: string =
+    ret.receipt?.contractAddress || '';
+  const managingDAOAddress = await getContractAddress('DAO', hre);
+  const pluginRepoFactoryAddress = await getContractAddress(
+    'PluginRepoFactory',
+    hre
+  );
+  const pluginRepoFactoryContract = await ethers.getContractAt(
+    'PluginRepoFactory',
+    pluginRepoFactoryAddress
+  );
+  await pluginRepoFactoryContract.createPluginRepoWithVersion(
+    'AddressListVoting',
+    [1, 0, 0],
+    addressListVotingManagerAddress,
+    '0x00',
+    managingDAOAddress
+  );
 };
 export default func;
 func.tags = ['AddressListVotingManager'];
--- packages/contracts/deploy/91_erc20voting-manager.ts
+++ packages/contracts/deploy/91_erc20voting-manager.ts
@@ -1,5 +1,6 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
+import {getContractAddress} from './helpers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts, ethers} = hre;
@@ -7,10 +8,29 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const {deployer} = await getNamedAccounts();
 
-  await deploy('Erc20VotingManager', {
+  const ret = await deploy('Erc20VotingManager', {
     from: deployer,
     log: true,
   });
+
+  // register the plugin
+  const erc20VotingManagerAddress: string = ret.receipt?.contractAddress || '';
+  const managingDAOAddress = await getContractAddress('DAO', hre);
+  const pluginRepoFactoryAddress = await getContractAddress(
+    'PluginRepoFactory',
+    hre
+  );
+  const pluginRepoFactoryContract = await ethers.getContractAt(
+    'PluginRepoFactory',
+    pluginRepoFactoryAddress
+  );
+  await pluginRepoFactoryContract.createPluginRepoWithVersion(
+    'Erc20Voting',
+    [1, 0, 0],
+    erc20VotingManagerAddress,
+    '0x00',
+    managingDAOAddress
+  );
 };
 export default func;
 func.tags = ['Erc20VotingManager'];
